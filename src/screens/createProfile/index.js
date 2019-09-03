ec986e6134927d3e232cc8333e2c364303705bfb
bokaderohit98 <bokaderohit98@gmail.com>
2019-09-04 02:18:59 +0530

    feat(profile-flow): added layout for save profile page

--- src/screens/createProfile/index.js
+++ src/screens/createProfile/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { IconButton } from 'react-native-paper';
+import { View, TouchableWithoutFeedback, Keyboard, Text } from 'react-native';
+import { IconButton, Button } from 'react-native-paper';
 import styled from 'styled-components';
 import tabsSchema from './tabsSchema';
 import ActionContainer from './ActionContainer';
@@ -15,6 +15,20 @@ const Container = styled.View`
     margin-bottom: 40px;
 `;
 
+const SaveContainer = styled.View`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    padding-right: 40px;
+`;
+
+const Message = styled.Text`
+    align-items: center;
+    font-size: 32px;
+    padding: 16px;
+`;
+
 class CreateProfile extends Component {
     constructor(props) {
         super(props);
@@ -26,6 +40,12 @@ class CreateProfile extends Component {
         };
     }
 
+    componentDidMount() {
+        const { tabs } = this.state;
+        const { required, value } = tabs[0];
+        if (required && value === '') this.setState({ nextDisabled: true });
+    }
+
     handleTabSwitch = event => {
         let { activeTabIndex, tabs } = this.state;
         let nextDisabled = false;
@@ -35,16 +55,31 @@ class CreateProfile extends Component {
             if (activeTabIndex === 0) prevDisabled = true;
         } else if (event === 'next') {
             activeTabIndex += 1;
-            if (activeTabIndex === tabs.length - 1) nextDisabled = true;
+            if (activeTabIndex !== tabs.length) {
+                const { required, value } = tabs[activeTabIndex];
+                if (required && value === '') nextDisabled = true;
+            }
         }
         this.setState({ activeTabIndex, prevDisabled, nextDisabled });
     };
 
     handleInputChange = value => {
         const { tabs, activeTabIndex } = this.state;
+        const nextDisabled = tabs[activeTabIndex].required && value === '';
         const updated = [...tabs];
         updated[activeTabIndex].value = value;
-        this.setState({ tabs: updated });
+        this.setState({ tabs: updated, nextDisabled });
+    };
+
+    renderSaveTab = () => {
+        return (
+            <SaveContainer>
+                <Message>Let's save your profile!</Message>
+                <Button mode="outlined" onPress={() => console.log('saved')}>
+                    Save Profile
+                </Button>
+            </SaveContainer>
+        );
     };
 
     render() {
@@ -62,18 +97,23 @@ class CreateProfile extends Component {
                         color="#6a1b9a"
                         onPress={() => this.handleTabSwitch('prev')}
                     />
-                    <ActionContainer
-                        activeTabIndex={activeTabIndex}
-                        tabs={tabs}
-                        handleInputChange={this.handleInputChange}
-                    />
-                    <IconButton
-                        disabled={nextDisabled}
-                        icon="keyboard-arrow-right"
-                        size={32}
-                        color="#6a1b9a"
-                        onPress={() => this.handleTabSwitch('next')}
-                    />
+                    {activeTabIndex < tabs.length && (
+                        <>
+                            <ActionContainer
+                                activeTabIndex={activeTabIndex}
+                                tabs={tabs}
+                                handleInputChange={this.handleInputChange}
+                            />
+                            <IconButton
+                                disabled={nextDisabled}
+                                icon="keyboard-arrow-right"
+                                size={32}
+                                color="#6a1b9a"
+                                onPress={() => this.handleTabSwitch('next')}
+                            />
+                        </>
+                    )}
+                    {activeTabIndex === tabs.length && this.renderSaveTab()}
                 </Container>
             </TouchableWithoutFeedback>
         );
